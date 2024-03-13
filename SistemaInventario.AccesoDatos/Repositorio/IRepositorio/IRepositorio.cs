d843cd013558411275eb2a1cfcfa0024cf9afc14
AndreaAvalos2 <sara3478236@gmail.com>
2024-03-13 07:45:52 -0600

    Patron de repositorio

--- SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -10,7 +10,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
     public interface IRepositorio<T> where T : class
     {
         // Retorna tipo de dato generico cuando se lance la funcion obtener
- Task<T> obtener(int id); // Busqueda directa por identificador
+        Task<T> obtener(int id); // Busqueda directa por identificador
                          // Obtener todos una lista de elementos
         Task<IEnumerable <T>> ObtenerTodos(
             //Parametro a recibir "Expresion", si no se le manda ningun dato se pone como nulo
@@ -28,8 +28,8 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
                 bool isTracking = true
             );
         // Agregar
-         void Task<Agregar>(T entidad);
-        // Remover
+          void Task<Agregar>(T entidad);
+        // Remover   
         void Remover(T entidad);
 
         void RemoverRango(IEnumerable<T> entidad);
---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IUnidadTrabajo : IDisposable //Libreria para deja en memoria que se utiliza constantemente
+    {
+        //Aqui se van a agregar todos los modelos 
+        IBodegaRepositorio Bodega { get; }
+
+        //Tarea asincrona
+        Task Guardar();
+
+
+    }
+}
